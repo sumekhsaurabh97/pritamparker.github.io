ac1a56bc4957f7c5b6adf5aa6923719bd01f9a6b
Pritam Parker <pritamparker@gmail.com>
2020-01-23 21:26:04 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var circleArray = []
-var viewportHeight = window.innerHeight
-var viewportWidth = window.innerWidth
+var circleArray = [];
+var viewportHeight = window.innerHeight;
+var viewportWidth = window.innerWidth;
 
 var colorArray = [
   '#D53939',
@@ -8,11 +8,11 @@ var colorArray = [
   '#FFEA85',
   '#DBE2EF',
   '#2A363B',
-  '#53354A',
-]
+  '#53354A'
+];
 
 function setBannerViewPort() {
-  $('.vetti-scene').height(window.innerHeight)
+  $('.vetti-scene').height(window.innerHeight);
 }
 
 function getRandom(min, max) {
@@ -21,52 +21,57 @@ function getRandom(min, max) {
 
 function randomCircle() {
   if (circleArray.length > 3) {
-    removableNode = circleArray.splice(0, 1)[0]
-    $('#' + removableNode).remove()
+    removableNode = circleArray.splice(0, 1)[0];
+    $('#' + removableNode).remove();
   }
-  var id = (+new Date).toString(36) + Math.random().toString(36).slice(2, 7)
-  circleArray.push(id)
-  $('.vetti-scene')
-    .append('<div class="rangoli-vattam" id="' + id + '"></div>')
-  window.setTimeout(function () {
-    const posX = getRandom(0, viewportWidth)
-    const posY = getRandom(0, viewportHeight)
+  var id =
+    (+new Date()).toString(36) +
+    Math.random()
+      .toString(36)
+      .slice(2, 7);
+  circleArray.push(id);
+  $('.vetti-scene').append(
+    '<div class="rangoli-vattam" id="' + id + '"></div>'
+  );
+  window.setTimeout(function() {
+    const posX = getRandom(0, viewportWidth);
+    const posY = getRandom(0, viewportHeight);
     $('#' + id).css({
       left: posX + 'px',
       top: posY + 'px',
       height: viewportHeight * 2,
       width: viewportHeight * 2,
       'background-color': colorArray[getRandom(0, colorArray.length - 1)],
-      transform: 'scale3d(3, 3, 3)',
-    })
-  }, 100)
+      transform: 'scale3d(3, 3, 3)'
+    });
+  }, 100);
 
-  window.setTimeout(randomCircle, 10000)
+  window.setTimeout(randomCircle, 10000);
 }
-$(window).on('load', function () {
-  setBannerViewPort()
-  $(window).resize(setBannerViewPort)
-  $('.percentage-container').addClass('no-width')
-  setTimeout(function () {
-    window.clearInterval(id)
-    $('.peru').text('Pritam Parker')
-    $('.seira-vela').text('JavaScript Developer')
-    $('body').css('overflow', 'auto')
-    $('.rendu').css('display', 'block')
-    $('.down').css('opacity', 1)
-    window.setTimeout(randomCircle, 1000)
-  }, 1500)
+$(window).on('load', function() {
+  setBannerViewPort();
+  $(window).resize(setBannerViewPort);
+  $('.percentage-container').addClass('no-width');
+  setTimeout(function() {
+    window.clearInterval(id);
+    $('.peru').text('Lakshita Rajput');
+    $('.seira-vela').text('Accept My Instagram Request :)');
+    $('body').css('overflow', 'auto');
+    $('.rendu').css('display', 'block');
+    $('.down').css('opacity', 1);
+    window.setTimeout(randomCircle, 1000);
+  }, 1500);
 
-  $(document).scroll(function () {
-    var imgOffset = $('.enaku-therunjathu>h2').offset().top
-    var scroll = $(document).scrollTop()
-    var hasClass = $('.percentage-container').hasClass('no-width')
-    if (imgOffset < (scroll + (window.innerWidth > 1200 ? 350 : 150))) {
-      $('.percentage-container').removeClass('no-width')
+  $(document).scroll(function() {
+    var imgOffset = $('.enaku-therunjathu>h2').offset().top;
+    var scroll = $(document).scrollTop();
+    var hasClass = $('.percentage-container').hasClass('no-width');
+    if (imgOffset < scroll + (window.innerWidth > 1200 ? 350 : 150)) {
+      $('.percentage-container').removeClass('no-width');
     }
-  })
+  });
 
-  if (window.navigator.platform.match(/win/ig)) {
-    $('body').addClass('windows')
+  if (window.navigator.platform.match(/win/gi)) {
+    $('body').addClass('windows');
   }
-})
+});
